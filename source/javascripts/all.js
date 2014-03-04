cad7aef54c49277a67392f4d603f744d766f512e
Qiu Juntao <juntao.qiu@gmail.com>
2014-03-05 01:23:59 +1100

    initial gh-pages commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,8 @@
+//= require jquery/dist/jquery
+//= require underscore/underscore
+//= require openlayers/OpenLayers
+//= require heatmap.js/src/heatmap
+//= require heatmap.js/src/heatmap-openlayers
+//= require foundation/js/foundation.min
+
+//= require app
