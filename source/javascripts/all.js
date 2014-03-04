62642bf7ad519354641b784604f52b7eaa20afbf
Qiu Juntao <juntao.qiu@gmail.com>
2014-03-05 01:30:52 +1100

    Site updated to c970f76

--- source/javascripts/all.js
+++
@@ -1,8 +0,0 @@
-//= require jquery/dist/jquery
-//= require underscore/underscore
-//= require openlayers/OpenLayers
-//= require heatmap.js/src/heatmap
-//= require heatmap.js/src/heatmap-openlayers
-//= require foundation/js/foundation.min
-
-//= require app
